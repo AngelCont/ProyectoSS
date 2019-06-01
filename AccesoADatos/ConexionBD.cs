e5da3e9dbf0cd676f27df3cdf855121fcd1a471c
Jose Angel Contreras Ramos <angel.con3111@gmail.com>
2019-06-01 12:34:35 -0500

    Definicion de la clase para el acceso a datos

---
+++ AccesoADatos/ConexionBD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccesoDeDatos
+{
+    static class Constantes
+    {
+        public static string connectionString = @"Data Source=Maruri-PC;Initial Catalog=ServicioSocial;User ID=adminSS;Password=***********";
+    }
+
+    public class ConexionBD
+    {
+        private SqlConnection sqlConnection;
+        private String conexionString;
+        public ConexionBD()
+        {
+            sqlConnection = new SqlConnection(conexionString);
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection != null)
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+    }
+}
